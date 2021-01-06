3b62d3e7864e7c477ec37ef5b28b52267fd6b66b
Lucas <lucas.acdc92@gmail.com>
2021-01-05 21:32:52 -0300

    Portfolio finished

--- script.js
+++ script.js
@@ -27,13 +27,13 @@ $(document).ready(function(){
    //typing animation script
 
    var typed = new Typed(".typing", {
-       strings: ["Developer", "Designer", "Freelancer", "Enthusiastic"],
+       strings: ["Developer", "Designer", "Freelancer", "Passionate"],
        typeSpeed: 100,
        backSpeed: 60,
        loop: true,
    })
    var typed = new Typed(".typing-2", {
-    strings: ["Developer", "Designer", "Freelancer", "Enthusiastic"],
+    strings: ["Developer", "Designer", "Freelancer", "Passionate"],
     typeSpeed: 100,
     backSpeed: 60,
     loop: true,
